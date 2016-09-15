628d82d47f5ba1b52c69a8ae3a299a9537deba02
U-LOHIKA\pkolevatyh <pkolevatyh@ws-pkolevatyh.lohika.com>
2016-09-15 14:50:00 +0300

    Few routes added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-#[macro_use]
-extern crate nickel;
+extern crate rustc_serialize;
+#[macro_use] extern crate nickel;
 
-use nickel::Nickel;
+use nickel::{Nickel, HttpRouter, JsonBody};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct User {
+    name: String,
+    pass:  String,
+}
 
 fn main() {
     let mut server = Nickel::new();
-    server.utilize(router! { get "**" => |_req, _res| { "Hello world!"  }  });
+    server.get("/bar", middleware!("This is the /bar handler"));
+    server.get("/user/:userid", middleware! { |request| format!("This is user: {:?}", request.param("userid"))  });
+    server.get("/a/*/d", middleware!("matches /a/b/d but not /a/b/c/d"));
+    server.get("/a/**/d", middleware!("This matches /a/b/d and also /a/b/c/d"));
+
+
+    server.post("/post/request", middleware! { |request, response|
+        let user = request.json_as::<User>().unwrap();
+        format!("Hello {}", user.name)
+    });
+
     server.listen("127.0.0.1:6767");
 }
